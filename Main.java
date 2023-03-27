7d2831c39fdcebb7aa95fd4fd8e17b68377a531b
ahmeddmk <96780604+ahmeddmk@users.noreply.github.com>
2023-03-27 17:07:53 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main (
     public static void main (string[] args ) { 
         System.out.println("hello world"); 
+        System.out.println("Welcome to TalanTunisie"); 
+
     }
   }
